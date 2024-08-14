9dd2bb39d6a56bbc63b468fa9fc12f9e7392f9d9
vitoriacezario <103068182+vihh123@users.noreply.github.com>
2024-08-14 10:05:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
