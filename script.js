78a04fe4ff59a8160094075ec7ddb87e2ea24b05
vitoriacezario <103068182+vihh123@users.noreply.github.com>
2024-08-14 09:21:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,73 +6,72 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Voce precisa de algum atendimento? ",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-            texto: "SIM eu preciso!",
-            afirmacao: " voce precisa de atendimento nesse hospital"
+                texto: "Isso é assustador!",
+                afirmacao: "afirmação"
             },
             {
-            texto: "Não, apenas quero conhecer como funciona!",
-            afirmacao: " estou apenas conhecendo" 
+                texto: "Isso é maravilhoso!",
+                afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "GOSTARIA DE MARCAR ALGUMA CONSULTA? ",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-            texto: "SIM!",
-            afirmacao: " GOSTARIA DE SER ATENDIDO(A)"
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "afirmação"
             },
             {
-            texto: "Não, apenas quero conhecer como funciona!",
-            afirmacao: " estou apenas conhecendo" 
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "afirmação"
             }
         ]
     },
-     enunciado: "Voce esta a prcura de algum especialista? ",
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-            texto: "SIM estou !",
-            afirmacao: " voce esta a procura de algum medico(a) especifico?"
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "afirmação"
             },
             {
-            texto: "voce esta a procura de algum especialista!",
-            afirmacao: " estou apenas conhecendo" 
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "afirmação"
             }
         ]
     },
-        ]
-    },
-    {enunciado: "conhece nosso hospital? ",
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-            texto: "SIM!",
-            afirmacao: " voce esta a procura de algum medico(a) especifico?"
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "afirmação"
             },
             {
-            texto: "voce conhece nosso hospital!",
-            afirmacao: " ainda nao mas gostaria de mais informaçao" 
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "afirmação"
             }
         ]
     },
-        ]
-    },
-    enunciado: "posso ajudar mais em alguma coisa? ",
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
-            texto: "SIM  !",
-            afirmacao: " posso ajudar em mais alguma coisa?"
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "afirmação"
             },
             {
-            texto: "posso ajudar mais em alguma coisa!",
-            afirmacao: " nao obrigado(a) estou satisfeito" 
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "afirmação"
             }
         ]
     },
-        ]
-    },
+];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -106,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2024...";
+    caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
