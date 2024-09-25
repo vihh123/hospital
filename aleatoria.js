457caba55a324b7bf173b7bc5bfc90ce2eb780e1
Yohana <117214321+yohanablack@users.noreply.github.com>
2024-09-25 09:18:48 -0300

    Criando pasta

--- aleatoria.js
+++ aleatoria.js
@@ -1,4 +1,8 @@
+const nomes = ["Fabiane", "Marcia", "Danielly", "Gabriel", "Amanda", "Guilherme", "Yohana"];
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes)
