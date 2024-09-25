4d89f6fd5e0577964fff1b7b88bb05917f997f9b
Yohana <117214321+yohanablack@users.noreply.github.com>
2024-09-25 09:54:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,77 +1,12 @@
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-    {
-        enunciado: "Bem vindo(a) podemos te ajudar?",
-        alternativas: [
-            {
-                texto: "sim eu gostaria!",
-                afirmacao: "afirmacao. "
-            },
-            {
-                texto: "No momento Não!",
-                afirmacao: "afirmacao."
-            }
-        ]
-    },
-    {
-        enunciado: "Voce ja ouviu falar do Hospital?",
-        alternativas: [
-            {
-                texto: "Sim, ja ouvi falar.",
-                afirmacao: "afirmacao."
-            },
-            {
-                texto: "No momento não.",
-                afirmacao: "adfirmacao."
-            }
-        ]
-    },
-    {
-        enunciado: "Voce esta a procura de algum expecialista expecifico?",
-        alternativas: [
-            {
-                texto: "SIM.",
-                afirmacao: "afirmacao."
-            },
-            {
-                texto: "NAO.",
-                afirmacao: "afirmacao."
-            }
-        ]
-    },
-    {
-        enunciado: "Gostaria de marcar alguma consulta?",
-        alternativas: [
-            {
-                texto: "SIM, gostaria.",
-                afirmacao: "afirmacao."
-            },
-            {
-                texto: "Nao.",
-                afirmacao: "afirmacao!"
-            }
-        ]
-    },
-    {
-        enunciado: "Oque esta achando do nosso atendimento? ",
-        alternativas: [
-            {
-                texto: "Excelente.",
-                afirmacao: "afirmacao."
-            },
-            {
-                texto: "Poderia Melhorar!.",
-                afirmacao: "afirmacao. "
-            }
-        ]
-    },
-];
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -88,8 +23,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
@@ -98,16 +33,33 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
+function jogaNovamente() {
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function substituiNome() {
+    for (const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+
+substituiNome();
+substituiNome();
 mostraPergunta();
