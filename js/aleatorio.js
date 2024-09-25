bb42f24d311e1cb15c0c94f42328491d9a1263a1
Yohana <117214321+yohanablack@users.noreply.github.com>
2024-09-25 10:03:54 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fabiane", "Marcia", "Danielly", "Gabriel", "Amanda", "Guilherme", "Yohana"];
+const nomes = ["Fabiano", "Marcia", "Danielly", "Gabriel", "Amanda", "Guilherme", "Yohana"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
