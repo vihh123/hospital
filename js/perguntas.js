e9f6dd3726c21b1ddee06ec7a262ab3951900f23
Yohana <117214321+yohanablack@users.noreply.github.com>
2024-10-09 09:29:39 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -26,9 +26,9 @@ export const perguntas = [
             {
                 texto: "Meu nome é (seu nome), nasci em (data de nascimento).",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "Com ele temos acesso á Saúde.",
+                    "e um otimo atendimento.",
+                    "Um bom atendimento médico deve ser acessível a todos. Isso significa não apenas ter unidades de saúde disponíveis, mas também garantir que as pessoas possam obter atendimento adequado sem barreiras financeiras ou geográficas. A promoção do acesso à saúde é vital para a equidade social."
                 ],
                 proxima: 3,
             },
@@ -49,18 +49,18 @@ export const perguntas = [
             {
                 texto: "Estou sentindo (seu sintoma)",
                 afirmacao: [
-                    "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "há tambem Comunicação Clara.",
+                    "A comunicação é fundamental no atendimento médico. Médicos e pacientes devem manter um diálogo aberto e claro, onde informações sobre sintomas, opções de tratamento e possíveis riscos sejam discutidas.",
+                    " Isso empodera os pacientes a tomar decisões informadas sobre sua saúde e aumenta a adesão aos tratamentos recomendados."
                 ],
                 proxima: 3,
             },
             {
                 texto: "Eu quero um medicamento/consulta",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                    "e a Prevenção como Prioridade.",
+                    "A prevenção é uma das melhores estratégias para manter a saúde. Consultas regulares, exames preventivos e orientações sobre hábitos saudáveis podem evitar o surgimento de doenças.",
+                    "Um bom atendimento médico inclui a promoção de práticas de vida saudáveis, que podem ter um impacto significativo na saúde pública.."
                 ],
                 proxima: 4,
             },
@@ -72,16 +72,16 @@ export const perguntas = [
             {
                 texto: "Ok vou aguardar.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "e o Trabalho em Equipe.",
+                    "Também temos a  Humanização do Atendimento."
                 ],
                 proxima: 5,
             },
             {
                 texto: "Quero ser atendido agora.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "A humanização no atendimento médico é essencial. Profissionais de saúde devem ser empáticos e compreender as necessidades emocionais dos pacientes. Isso inclui ouvir atentamente, oferecer apoio e respeitar a dignidade de cada indivíduo.",
+                    "O atendimento médico eficaz muitas vezes envolve um trabalho colaborativo entre diferentes profissionais da saúde, como médicos, enfermeiros, nutricionistas e psicólogos."
                 ],
                 proxima: 6,
             },
@@ -93,16 +93,16 @@ export const perguntas = [
             {
                 texto: "Estou com (seu sintoma).",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Um aspecto muitas vezes negligenciado é a educação em saúde. O atendimento médico eficaz não se resume apenas ao tratamento de doenças, mas também envolve a orientação sobre prevenção.",
+                    "E há Educação e Prevenção."
                 ],
                 proxima: 5,
             },
             {
                 texto: "Quero um medicamento/consulta (nome do remedio) para meu sintoma.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "e Desafios e Oportunidades",
+                    "Apesar de seu papel essencial, o atendimento médico enfrenta diversos desafios, como a escassez de profissionais, a alta demanda por serviços e a necessidade de inovação constante. No entanto, esses desafios também oferecem oportunidades para melhorias. "
                 ],
                 proxima: 6,
             },
@@ -114,16 +114,16 @@ export const perguntas = [
             {
                 texto: "Começou há cerca de uma hora.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Investimentos em tecnologia, capacitação profissional e infraestrutura são passos cruciais para superar barreiras e aprimorar a qualidade do atendimento.",
+                    "Por isso é bom marcar uma consulta."
                 ],
                 proxima: 7,
             },
             {
                 texto: "Quero (nome do medicamento)",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "E tem a Integralidade do Cuidado",
+                    "Um atendimento médico eficaz deve considerar a integralidade do paciente. Isso significa que os profissionais devem avaliar não apenas a condição física, mas também os aspectos emocionais e sociais que podem influenciar a saúde."
                 ],
                 proxima: 7,
             },
@@ -135,16 +135,16 @@ export const perguntas = [
             {
                 texto: "Não, nunca tive problemas assim..",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Tambem temos a Tecnologia e Inovação",
+                    "A tecnologia tem revolucionado o atendimento médico. Telemedicina, prontuários eletrônicos e inteligência artificial estão tornando o diagnóstico e o tratamento mais eficientes."
                 ],
                 proxima: 7,
             },
             {
                 texto: "Sim sempre tive (problema relacionado ao sintoma).",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    " sem contar os Desafios do Atendimento Médico",
+                    "Apesar dos avanços, o atendimento médico enfrenta desafios, como a sobrecarga de profissionais, falta de recursos e desigualdade no acesso."
                 ],
                 proxima: 7,
             },
@@ -163,9 +163,11 @@ export const perguntas = [
             {
                 texto: "Obrigada(O) pelo atendimento.",
                 afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                    "O atendimento médico é uma responsabilidade compartilhada entre profissionais de saúde, instituições e a sociedade. Investir em um sistema de saúde que priorize a qualidade, a humanização e a prevenção é fundamental para promover uma vida mais saudável e digna para todos.",
+                    "O atendimento médico é um elemento-chave para a promoção da saúde e o bem-estar da população. Ao focar na integralidade, comunicação, prevenção e trabalho em equipe, é possível construir um sistema de saúde mais eficaz e humano, que atenda às necessidades de todos os cidadãos.
+
+.",
+                    "Todos precisamos de um bom atendimento."
                 ],
             },
         ]
